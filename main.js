330939c9fe97d376dd7681c2c3194fb4e98bd672
Reseanne Rampou <reseanner@gmail.com>
2025-11-07 15:04:15 +0200

    Fix paths and add Directors example

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-var director1 = {
+// Define the Director object (interfaces disappear after compilation)
+const director1 = {
     name: "Alice",
     subject: "Math",
     yearsOfExperience: 10,
     numberOfReports: 5,
-    printNumberOfReports: function () {
-        console.log("I have ".concat(this.numberOfReports, " reports."));
+    printNumberOfReports() {
+        console.log(`I have ${this.numberOfReports} reports.`);
     }
 };
+
+// Call the method
 director1.printNumberOfReports();
+
